88e8177166b387ea90fb8b3bddb436fe6f66da71
Muayminly <145211564+Muayminly@users.noreply.github.com>
2024-10-16 16:24:43 +0700

    v1 Add API TU

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,21 @@ function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('/api/auth', {
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU5d9c2f64dba5a79b5703f16217099f581aa997f95cea8f85ee3303e96d2af4fa9389bf05fe88938703a7a4a10e198098'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({"UserName" : "username","PassWord" : "password"})
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('message').innerText = data.message;
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {console.error('Error:', error);
+        document.getElementById('message').innerText = error.message;
+    });
 }
 
 
