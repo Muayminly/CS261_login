0ebe4d21fdabd0f16c62896530c79679a10af06b
Muayminly <145211564+Muayminly@users.noreply.github.com>
2024-10-18 14:28:42 +0700

    role+css

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,10 @@ function submitLogin() {
             'Content-Type': 'application/json',
             'Application-Key': 'TU5d9c2f64dba5a79b5703f16217099f581aa997f95cea8f85ee3303e96d2af4fa9389bf05fe88938703a7a4a10e198098'
         },
-        body: JSON.stringify({"UserName" : "username","PassWord" : "password"})
+        body: JSON.stringify({
+            "UserName": username,
+            "PassWord": password
+        })
     })
     .then(response => response.json())
     .then(data => {
@@ -19,21 +22,18 @@ function submitLogin() {
     });
 }
 
+// Select the password input and toggle icon
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
 
+// Add click event to the icon
+togglePassword.addEventListener('click', function () {
+    // Toggle the password visibility
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
 
-function call_REST_API_Hello() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    // Toggle the eye / eye-slash icon
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+});
 
-    const url = (
-        'http://localhost:8080/hello?' +
-        new URLSearchParams({ myName: username, lastName: password}).toString()
-      );
-    
-    fetch(url)
-    .then(response => response.text())
-    .then(text => {
-        document.getElementById('message').innerText = text;
-    })
-    .catch(error => console.error('Error:', error));
-}
